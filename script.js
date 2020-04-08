1fe660b66ce915bfaccc32261db066106b5fd5df
Muneerah1612 <61384690+Muneerah1612@users.noreply.github.com>
2020-04-08 22:29:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const  data = [
     { Principal: 2000, time : 3} ] ;
     
 function interestCalculator (array) {
-    let interestdata= [];
+    let interestData= [i];
     array. forEach ((i)=> {
-        if (i.Principal >= 2500 && (i.time >1 && i.time <3)) {i.rate = 3;}
+        if (i.Principal >= 2500 && (i.time >1 && i.time (3)) {console.log ("i.rate = 3");}
         
-        else if (i.Principal >= 2500 && (i.time >=3)) {i.rate=4;}
-        else if (i.Principal < 2500 || (i.time <=1)) {i.rate =2;}
-        else {i.rate =1}
+        else if (i.Principal >= 2500 && (i.time >=3)) {console.log ("i.rate=4");}
+        else if (i.Principal < 2500 || (i.time <=1)) {console.log ("i.rate =2");}
+        else {console.log ("i.rate =1");}
         interest = (i.Principal * i.rate * i.time) / 100;
             interestdata.push ({`Principal`: Principal, `rate`: i.rate, `time` : i.time,
              `interest` : i.interest});}
 
-             console.log (interestdata);
-             return interestdata;
+             console.log (interestData);
+             return interestData;
     }
     interestCalculator (data);
    
